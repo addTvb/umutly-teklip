4134ef34c28dcc2802ceee44cac868fe3b0d9f8b
Арслан Абаев <abayew.arslan@gmail.com>
2021-05-29 10:07:58 +0300

    ✨feat( ): change  directory structure

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
-import { useEffect, useState } from "react";
+import { Header } from "./components/Header";
+
 import "./App.css";
 
 function App() {
-    const [nasaData, setNasaData] = useState({
-        hdurl: "",
-    });
-
-    useEffect(() => {
-        const getImage = () => {
-            return fetch(
-                "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY",
-                {
-                    method: "GET",
-                    accept: "*/*",
-                }
-            ).then((response) => {
-                response.text().then((text) => {
-                    const data = text && JSON.parse(text);
-                    console.log("data", data);
-                    return data;
-                });
-            });
-        };
-        setNasaData(getImage());
-        console.log(getImage());
-    }, []);
     return (
-        <div className="App">
-            <img src={nasaData.hdurl} />
+        <div>
+            <Header />
+            Hello
         </div>
     );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import { memo } from 'react'
+
+import './Header.css'
+
+export default memo(function Header() {
+    return (
+        <div>
+            Header     
+        </div>
+    )
+})
